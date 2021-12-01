264de1c2929361327f954664158c59fa75d9845c
Jodu555 <Jodu505@gmail.com>
2021-12-01 06:37:33 +0100

    added the --y and error shown

--- src/index.js
+++ src/index.js
@@ -3,26 +3,32 @@ const process = require('process');
 const inquirer = require('inquirer');
 
 async function start() {
-    await inquirer
-        .prompt([
-            {
-                type: 'confirm',
-                name: 'morgan',
-                message: 'Do you want morgan',
-                default: true,
-            }
-        ])
-        .then((answers) => {
-            console.log(answers);
-            // Use user feedback for... whatever!!
-        })
-        .catch(errorCatch);
+    console.log(process.argv);
+    if (process.argv.includes('--y')) {
+
+    } else {
+        await inquirer
+            .prompt([
+                {
+                    type: 'confirm',
+                    name: 'morgan',
+                    message: 'Do you want morgan',
+                    default: true,
+                }
+            ])
+            .then((answers) => {
+                console.log(answers);
+                // Use user feedback for... whatever!!
+            })
+            .catch(errorCatch);
+    }
 }
 
 
 
 const errorCatch = (error) => {
     if (error.isTtyError) {
+        console.error('Your System does not support this type of Terminal interaction!');
         // Prompt couldn't be rendered in the current environment
     } else {
         // Something else went wrong
