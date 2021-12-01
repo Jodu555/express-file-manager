229c7c2fa8fd78f581d2f31bee93ec6eac158a85
Jodu555 <Jodu505@gmail.com>
2021-12-01 06:47:25 +0100

    wrote out the basic info about how the info schould be structured

--- src/ExpressApp.js
+++ src/ExpressApp.js
@@ -1,8 +1,28 @@
+const fs = require('fs');
+
 class ExpressApp {
     constructor(options) {
         this.options = options;
+        this.information = {
+            entry: 'src/index.js',
+            database: true, //Uses the @jodu555/mysqlapi api
+            authentication: {
+
+            },
+            routes: [
+                {
+                    name: 'todo',
+                }
+            ]
+        };
     }
     create() {
+
+    }
+    saveInfo() {
+
+    }
+    loadInfo() {
 
     }
 }
