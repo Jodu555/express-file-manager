124ddbac8433215a592fad3e3b49180881ad3b62
Jodu555 <Jodu505@gmail.com>
2021-12-01 09:03:42 +0100

    added the check if a config already exists

--- src/ExpressApp.js
+++ src/ExpressApp.js
@@ -20,6 +20,9 @@ class ExpressApp {
     create() {
 
     }
+    update() {
+
+    }
     saveInfo() {
 
     }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const process = require('process');
 const inquirer = require('inquirer');
+const fs = require('fs');
 const ExpressApp = require('./ExpressApp');
 
 async function start() {
@@ -10,24 +11,28 @@ async function start() {
         helmet: true,
         cors: true
     };
-    if (process.argv.includes('--y')) {
-        const app = new ExpressApp(options);
-        app.create();
+    if (!fs.existsSync('./describe.json')) {
+        if (process.argv.includes('--y')) {
+            const app = new ExpressApp(options);
+            app.create();
+        } else {
+            await inquirer
+                .prompt([
+                    {
+                        type: 'confirm',
+                        name: 'morgan',
+                        message: 'Do you want morgan',
+                        default: true,
+                    }
+                ])
+                .then((answers) => {
+                    console.log(answers);
+                    // Use user feedback for... whatever!!
+                })
+                .catch(errorCatch);
+        }
     } else {
-        await inquirer
-            .prompt([
-                {
-                    type: 'confirm',
-                    name: 'morgan',
-                    message: 'Do you want morgan',
-                    default: true,
-                }
-            ])
-            .then((answers) => {
-                console.log(answers);
-                // Use user feedback for... whatever!!
-            })
-            .catch(errorCatch);
+
     }
 }
 
